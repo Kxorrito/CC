897e8fc8f987a34e8a3bec319477b37dfa9c4edd
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-27 09:55:04 -0500

    operaciones

---
+++ operaciones/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main()
+{
+    int x=7;
+    int y=2;
+    int z=x%y;
+    int a=x/y;
+    std::cout<<z<<a;
+}
+*/
+/*
+int main(){
+int a=5;
+int b=6;
+int c=7;
+int d=a+b*c;
+std::cout<<d;
+}
+*/
+/*
+int main(){
+int a=5;
+int b=2;
+int c=4;
+int d=6;
+int e=8;
+int m= a+b+c+d+e/5;
+std::cout<<m;
+}
+*/
+/*
+int main(){
+int a=5;
+int b=2;
+int c=4;
+int d=6;
+int e=8;
+int f=7;
+int x=a*b%c+d/e-f;
+std::cout<<x;
+}
+*/
+/*
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b){
+        if(a>c){
+            cout<<a;
+        }
+        else{
+            cout<<a;
+        }
+    }
+        else{
+            if(b>a){
+                cout<<b;
+            }
+            else{
+                cout<<a;
+            }
+        }
+}
+*/
+
+
+
+
