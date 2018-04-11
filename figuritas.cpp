d5d00ed71f79a31400b599ee413374460e4bba6e
Kxorrito <31105075+Kxorrito@users.noreply.github.com>
2018-04-11 08:53:31 -0500

    Trabajos en laboratorio

---
+++ figuritas.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*float x,y,z,d;
+	int s,r,m;
+	
+	cout<<"ingresa el valor de x: ";
+	cin>>x;
+	cout<<"ingresa el valor de y: ";
+	cin>>y;
+	cout<<"ingresa el valor de z: ";
+	cin>>z;
+	
+	s=x+y+z;
+	r=x-y-z;
+	m=x*y*z;
+	d=x/y/z;
+	
+	cout<<"el primer numero es: "<<x<<endl;
+	cout<<"segundo numero es: "<<y<<endl;
+	cout<<"la suma es: \t\t"<<s<<'\n';
+	cout<<"la resta es: "<<r<<endl;
+	cout<<"la multiplicacion es: \t"<<m<<'\n';
+	cout<<"la division es: "<<d<<endl;*/
+	
+	
+	cout<<"*********"<<'\t'<<"  ***"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<" *"<<"   *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<"*"<<"     *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<"*"<<"     *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<"*"<<"     *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<"*"<<"     *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<"*"<<"     *"<<'\n';
+	cout<<"*\t"<<"*"<<'\t'<<" *"<<"   *"<<'\n';
+	cout<<"*********"<<'\t'<<"  ***"<<'\n';
+	
+	
+	cout<<"\t  *"<<'\n';
+	cout<<"\t ***"<<'\n';
+	cout<<"\t*****"<<'\n';
+	cout<<"\t"<<"  *"<<'\n';
+	return 0;
+}
+
